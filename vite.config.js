e0c7ad695b9c65ec4ff0e891058da4c53960d411
Abhay Kumar Singh <singh957672@gmail.com>
2025-01-31 13:31:58 +0530

    Add homepage for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/text-react-portfolio/' // GitHub repository name
+  base: '/text-react-portfolio/' 
 });
