0f1f327e726ad3466a6accfa436c12649066be68
Abhay Kumar Singh <singh957672@gmail.com>
2025-01-31 19:26:01 +0530

    Updated UI and fixed deployment issues

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -10,7 +10,7 @@ const Services = () => {
     <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
     <h4 className="text-center mb-2 text-lg font-Ovo">What i offer</h4>
     <h2 className="text-center text-5xl font-Ovo">My services</h2>
-    <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
+    <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">I am a frontend developer from Bihar, India with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
 
     <div className="grid grid-cols-auto gap-6 my-10">
         <div className="border border-gray-400 rounded-lg px-8 py-12
